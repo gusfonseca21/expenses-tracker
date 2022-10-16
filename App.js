aa626b4bb69c630451790b83f57894d45ae44593
Gustavo Fonseca de Araújo <113441393+GustavoFonsecaCleverSystems@users.noreply.github.com>
2022-10-16 18:01:14 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AllExpensesScreen from "./screens/AllExpensesScreen";
+import RecentExpensesScreen from "./screens/RecentExpensesScreen";
+
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: {
+              backgroundColor: "#273c75",
+              borderTopWidth: 1,
+              borderTopColor: "#ccc",
+            },
+            tabBarActiveTintColor: "#e1b12c",
+            tabBarInactiveTintColor: "#ccc",
+            headerStyle: {
+              backgroundColor: "#273c75",
+              borderBottomWidth: 1,
+              borderBottomColor: "#ccc",
+            },
+            headerRight: () => (
+              <Ionicons name="add-outline" size={30} color="white" />
+            ),
+          }}
+          sceneContainerStyle={{
+            backgroundColor: "#192a56",
+          }}
+        >
+          <Tab.Screen
+            name="RecentExpenses"
+            component={RecentExpensesScreen}
+            options={{
+              title: "Despesas Recentes",
+              headerTintColor: "#e1b12c",
+              headerTitleAlign: "center",
+              tabBarIcon: ({ size, color }) => (
+                <Ionicons name="alarm-outline" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="AlExpenses"
+            component={AllExpensesScreen}
+            options={{
+              title: "Todas as Despesas",
+              headerTintColor: "#e1b12c",
+              headerTitleAlign: "center",
+              tabBarIcon: ({ size, color }) => (
+                <Ionicons name="calendar-outline" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
