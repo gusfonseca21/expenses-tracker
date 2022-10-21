641450cb9b2f76bffee31ab05d1956d86f650efb
Gustavo Fonseca de Araújo <gusfonseca.dev@gmail.com>
2022-10-20 22:53:01 -0300

    mais um commit :)

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 
 import { StatusBar } from "expo-status-bar";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -15,32 +15,88 @@ import { ExpenseDataContext } from "./context/ExpensesDataContext";
 import { MonthExpensesScreen } from "./screens/MonthExpensesScreen";
 import DetailedExpense from "./screens/DetailedExpense";
 
+import AppLoading from "expo-app-loading";
+
 export default function App() {
   const [ALL_EXPENSES, setAllExpenses] = useState([
-    { id: 1, description: "Livro", price: "12,99", date: 1663027200000 },
-    { id: 2, description: "Pastel", price: "4,99", date: 1663286400000 },
-    { id: 3, description: "Chinelo", price: "49,99", date: 1663372800000 },
-    { id: 4, description: "Remédio", price: "99,99", date: 1663632000000 },
-    { id: 5, description: "Cigarro", price: "5,00", date: 1664236800000 },
-    { id: 6, description: "Consulta", price: "200,00", date: 1664323200000 },
-    { id: 7, description: "Bar", price: "45,30", date: 1665446400000 },
-    { id: 8, description: "Uber", price: "25,25", date: 1665532800000 },
-    { id: 9, description: "Espanador", price: "20,99", date: 1665964800000 },
-    { id: 10, description: "Lápis", price: "2,00", date: 1666051200000 },
+    {
+      id: 1,
+      description: "Livro",
+      price: "12,99",
+      date: 1663027200000,
+      obs: "",
+    },
+    {
+      id: 2,
+      description: "Pastel",
+      price: "4,99",
+      date: 1663286400000,
+      obs: "",
+    },
+    {
+      id: 3,
+      description: "Chinelo",
+      price: "49,99",
+      date: 1663372800000,
+      obs: "",
+    },
+    {
+      id: 4,
+      description: "Remédio",
+      price: "99,99",
+      date: 1663632000000,
+      obs: "",
+    },
+    {
+      id: 5,
+      description: "Cigarro",
+      price: "5,00",
+      date: 1664236800000,
+      obs: "",
+    },
+    {
+      id: 6,
+      description: "Consulta",
+      price: "200,00",
+      date: 1664323200000,
+      obs: "",
+    },
+    { id: 7, description: "Bar", price: "45,30", date: 1665446400000, obs: "" },
+    {
+      id: 8,
+      description: "Uber",
+      price: "25,25",
+      date: 1665532800000,
+      obs: "",
+    },
+    {
+      id: 9,
+      description: "Espanador",
+      price: "20,99",
+      date: 1665964800000,
+      obs: "O corre corre da cidade grande",
+    },
+    {
+      id: 10,
+      description: "Lápis",
+      price: "2,00",
+      date: 1666051200000,
+      obs: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    },
   ]);
 
-  useEffect(() => {
-    setAllExpenses(ALL_EXPENSES.reverse());
-  }, [ALL_EXPENSES]);
-
-  const Tab = createBottomTabNavigator();
-  const Stack = createNativeStackNavigator();
-
-  const [isLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     "open-sans": require("./assets/fonts/Inter-Regular.ttf"),
     "open-sans-bold": require("./assets/fonts/Inter-Bold.ttf"),
     "open-sans-semi-bold": require("./assets/fonts/Inter-SemiBold.ttf"),
   });
+
+  useLayoutEffect(() => {
+    setAllExpenses(ALL_EXPENSES.reverse());
+  }, []);
+
+  const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator();
 
   const screenOptions = (icon, title) => {
     return {
@@ -52,6 +108,10 @@ export default function App() {
       ),
     };
   };
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   function MainTabNavigator() {
     return (
@@ -73,7 +133,20 @@ export default function App() {
             fontFamily: "open-sans-bold",
           },
           headerRight: () => (
-            <Ionicons name="add-outline" size={30} color="white" />
+            <Ionicons
+              name="add-outline"
+              size={30}
+              color="white"
+              style={{ right: 15 }}
+            />
+          ),
+          headerLeft: () => (
+            <Ionicons
+              name="bookmark-outline"
+              size={25}
+              color="white"
+              style={{ left: 15 }}
+            />
           ),
         }}
         sceneContainerStyle={{
@@ -116,6 +189,7 @@ export default function App() {
               headerTintColor: "#ecf0f1",
               headerTitleStyle: {
                 color: "#e1b12c",
+                fontFamily: "open-sans-bold",
               },
               headerTitleAlign: "center",
             }}
@@ -127,7 +201,11 @@ export default function App() {
               name="Home"
               component={MainTabNavigator}
             />
-            <Stack.Screen name="Details" component={DetailedExpense} />
+            <Stack.Screen
+              name="Details"
+              component={DetailedExpense}
+              options={{ headerTitle: "Detalhes" }}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </ExpenseDataContext.Provider>
--- screens/DetailedExpense.js
+++ screens/DetailedExpense.js
@@ -3,6 +3,9 @@ import React from "react";
 
 import { useContext } from "react";
 import { ExpenseDataContext } from "../context/ExpensesDataContext";
+import TextComponent from "../components/TextComponent";
+
+import { format } from "date-fns";
 
 export default function DetailedExpense({ route }) {
   const { ALL_EXPENSES } = useContext(ExpenseDataContext);
@@ -13,7 +16,48 @@ export default function DetailedExpense({ route }) {
 
   return (
     <View style={styles.rootView}>
-      <Text>{selectedExpense.description}</Text>
+      <View style={styles.expenseView}>
+        <TextComponent style={{ fontSize: 20, position: "absolute", top: 10 }}>
+          ---------------------------------
+        </TextComponent>
+        <TextComponent
+          style={{
+            fontFamily: "open-sans-semi-bold",
+            textAlign: "center",
+            fontSize: 26,
+          }}
+        >
+          {selectedExpense.description}
+        </TextComponent>
+        <TextComponent
+          style={{ fontSize: 16, textAlign: "center", marginTop: 30 }}
+        >
+          {format(selectedExpense.date, "dd/MM/yyyy")}
+        </TextComponent>
+        <TextComponent
+          style={{
+            fontSize: 24,
+            fontFamily: "open-sans-bold",
+            marginTop: 30,
+            textAlign: "center",
+          }}
+        >{`R$ ${selectedExpense.price}`}</TextComponent>
+        <TextComponent
+          style={{ fontFamily: "open-sans-semi-bold", marginTop: 30 }}
+        >
+          Observações:
+        </TextComponent>
+        <TextComponent style={{ textAlign: "justify", marginHorizontal: 15 }}>
+          {selectedExpense.obs === ""
+            ? "Não há observações para este gasto"
+            : selectedExpense.obs}
+        </TextComponent>
+        <TextComponent
+          style={{ fontSize: 20, position: "absolute", bottom: 10 }}
+        >
+          ---------------------------------
+        </TextComponent>
+      </View>
     </View>
   );
 }
@@ -22,5 +66,15 @@ const styles = StyleSheet.create({
   rootView: {
     borderTopColor: "#ccc",
     borderTopWidth: 1,
+    flex: 1,
+    position: "relative",
+  },
+  expenseView: {
+    marginHorizontal: 30,
+    marginVertical: 70,
+    backgroundColor: "#FDF5AB",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
